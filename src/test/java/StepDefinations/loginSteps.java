9ba48c07563d8f6027c7de408e65e8108f836245
OnkarKshirsagar1 <88650385+OnkarKshirsagar1@users.noreply.github.com>
2021-09-23 15:36:45 +0530

    OK

---
+++ src/test/java/StepDefinations/loginSteps.java
@@ -0,0 +1,32 @@
+package StepDefinations;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginSteps {
+
+	@Given("User is on login page")
+	public void user_is_on_ligin_page() {
+		System.out.println("Print Inside Statement= User is on login page");
+	}
+
+	@When("User enters username and password")
+	public void user_enters_username_and_password() {
+		System.out.println("Print Inside Statement= User enters username and password");
+	}
+
+	@And("Click on Login button")
+	public void click_on_login_button() {
+		System.out.println("Print Inside Statement= Click on Login button");
+	}
+
+	@Then("User is Navigated to Home Page")
+	public void user_is_navigated_to_home_page() {
+		System.out.println("Print Inside Statement= User enters username and password");
+	}
+
+
+
+}
